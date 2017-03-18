8017efea557381130999342a6e63f37254553df3
Teresa Jeon <teresajeon@seoulchristian.org>
2017-03-18 15:51:27 +0900

    Add comment

--- simpleCalc.py
+++ simpleCalc.py
@@ -1,15 +1,15 @@
-print "Hi! This is a simple calculator.\nYou need to type two numbers.\n"
+print "Hi! This is a simple calculator.\nYou need to type two numbers.\n" # print the string
 
-n1 = input("Type your first number: ")
-n2 = input("Type your second number: ")
+n1 = input("Type your first number: ") # set n1 to the input from the user
+n2 = input("Type your second number: ") # set n2 to the input from the user
 
-addition = n1 + n2
-subtraction = n1 - n2
-multiplication = n1 * n2
-division = n1 / n2
+addition = n1 + n2  # set addition as the sum of n1 and n2
+subtraction = n1 - n2 # set subtraction as the difference of n1 and n2
+multiplication = n1 * n2 # set multiplication as the product of n1 and n2
+division = n1 / n2 # set division as the quotient of n1 and n2
 
-print "\nThe results are:\n"
-print "%r + %r = " % (n1, n2), addition
-print "%r - %r = " % (n1, n2), subtraction
-print "%r * %r = " % (n1, n2), multiplication
-print "%r / %r = " % (n1, n2), division
+print "\nThe results are:\n" # print the string
+print "%r + %r = " % (n1, n2), addition # print the string and addition
+print "%r - %r = " % (n1, n2), subtraction # print the string and subtraction
+print "%r * %r = " % (n1, n2), multiplication # print the string and multiplication
+print "%r / %r = " % (n1, n2), division # print the string and division
